92657bf17240765c1655e8b9402e1472514eba1b
Yonsm <Yonsm@qq.com>
2020-06-11 20:25:32 +0800

    Add KODI

--- KODI/KODI.sh
+++ KODI/KODI.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if ! -z $1 ; then
+if [ ! -z $1 ] ; then
 	adb disconnect
 	adb connect $1
 fi
@@ -8,10 +8,8 @@ adb push addons.tar /sdcard
 adb shell "tar -x -f /sdcard/addons.tar -C /sdcard/Android/data/org.xbmc.kodi/files/.kodi/"
 adb shell "rm /sdcard/addons.tar"
 
-adb shell "ls -laR /sdcard/Android/data/org.xbmc.kodi/files/.kodi"
-
 adb push userdata.tar /sdcard
 adb shell "tar -x -f /sdcard/userdata.tar -C /sdcard/Android/data/org.xbmc.kodi/files/.kodi/"
 adb shell "rm /sdcard/userdata.tar"
 
-
+adb shell "ls -la /sdcard/Android/data/org.xbmc.kodi/files/.kodi/userdata"
